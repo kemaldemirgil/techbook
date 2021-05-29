25ab4bc77ce933d4808ae59370dc1075937eb451
Ashiemotto <ashheadley@gmail.com>
2021-05-29 11:00:11 -0400

    updated routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,13 @@
 //Imports............................................
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-
+const developerRoutes = require('./developer-routes');
+const recruiterRoutes = require('./recruiter-routes');
+const loginRoute = require('./login-routes');
 
 //Navigator..........................................
-router.use('/users', userRoutes);
-
+router.use('/users', developerRoutes);
+router.use('/recruiter',recruiterRoutes);
+router.use('/login',loginRoute)
 
 //Exports............................................
 module.exports = router;
