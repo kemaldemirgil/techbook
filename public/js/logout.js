9655e503e7cf5593d713fb3f7377130dd1d271cc
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-05-30 02:13:35 -0400

    adding profile

--- public/js/logout.js
+++ public/js/logout.js
@@ -12,11 +12,34 @@ const logout = async () => {
 };
 document.querySelector('#logout').addEventListener('click', logout);
 
+$( document ).ready(() => {
+  let badge = $(".badge");
+  console.log(badge)
+  if($(badge).text() === "Recruiter") {
+    $(badge).css("background-color", "purple")
+    $(badge).text("R");
+  }
+  if($(badge).text() === "Junior") {
+    $(badge).css("background-color", "green")
+    $(badge).text("JR");
+  }
+  if($(badge).text() === "Intermediate") {
+    $(badge).css("background-color", "blue")
+    $(badge).text("INT");
+  }
+  if($(badge).text() === "Senior") {
+    $(badge).css("background-color", "red")
+    $(badge).text("SR");
+  }
+});
+
 ///////////////////////////////////////////////////////////////////////
+
+
 
 const open = document.getElementById('open')
 const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const container = document.querySelector('.main-container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,11 @@
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
+var usersContainerEl = document.querySelector('.users-container');
+let navOption = document.querySelector(".nav-option");
 
 
-var formSubmitHandler = function () {
+$( document ).ready( () => {
+  console.log("fetching repos")
   var username = nameInputEl.innerHTML.trim();
     console.log(username);
   if (username) {
@@ -12,7 +15,7 @@ var formSubmitHandler = function () {
   } else {
     alert('Please enter a GitHub username');
   }
-};
+});
 
 var getUserRepos = function (user) {
   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
@@ -29,9 +32,9 @@ var getUserRepos = function (user) {
         alert('Error: ' + response.statusText);
       }
     })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
-    });
+  .catch(function (error) {
+    alert('Unable to connect to GitHub');
+  });
 };
 
 var displayRepos = function (repos) {
@@ -40,30 +43,43 @@ var displayRepos = function (repos) {
     return;
   }
   for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
 
+    var divEl = document.createElement('div');
+
+
+    var repoName = 'https://github.com/'+repos[i].owner.login + '/' + repos[i].name;
     var repoEl = document.createElement('a');
     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.handlebars?repo=' + repoName);
+
+    repoEl.setAttribute('href', repoName);
+    repoEl.setAttribute('target', "_blank");
 
     var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
+    titleEl.textContent = repos[i].name;
 
+    divEl.appendChild(titleEl);
     repoEl.appendChild(titleEl);
 
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
 
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-    repoEl.appendChild(statusEl);
-    repoContainerEl.appendChild(repoEl);
+
+
+
+    // var statusEl = document.createElement('span');
+    // statusEl.classList = 'flex-row align-center';
+
+    // if (repos[i].open_issues_count > 0) {
+    //   statusEl.innerHTML =
+    //     "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    // } else {
+    //   statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    // }
+    // repoEl.appendChild(statusEl);
+    divEl.appendChild(repoEl);
+    usersContainerEl.appendChild(divEl);
+    let repos = document.getElementById("myRepos");
+    let option = document.createElement('option');
+    option.innerHTML = repos[i].name;
+    repos.appendChild(option);
   }
 };
 
-const repoButton = document.querySelector(".get-repos-button");
-repoButton.addEventListener("click", formSubmitHandler);
