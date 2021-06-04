261a1f4b407a1b4672fb4fba91f0965b4a0233ac
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-04 02:09:20 -0400

    fixing bugs

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,6 +35,7 @@ router.post('/', async (req, res) => {
       req.session.firstlog = true;
       res.status(200).json(dbUserData);
       req.session.star = false;
+      req.session.experience = dbUserData.experience;
     });
   } catch (err) {
     console.log(err);
